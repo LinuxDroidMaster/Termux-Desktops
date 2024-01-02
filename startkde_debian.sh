abece0e6f8fd3a544fa691c6f4428b5987f4f5f8
DroidMaster <linuxdroidmaster@gmail.com>
2024-01-02 16:40:13 +0000

    create start kde

---
+++ startkde_debian.sh
@@ -0,0 +1,20 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Enable PulseAudio over Network
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+
+# Prepare termux-x11 session
+export XDG_RUNTIME_DIR=${TMPDIR}
+termux-x11 :0 >/dev/null &
+
+# Wait a bit until termux-x11 gets started.
+sleep 3
+
+# Login in PRoot Environment. Do some initialization for PulseAudio, /tmp directory
+# and run KDE as user droidmaster.
+# See also: https://github.com/termux/proot-distro
+# Argument -- acts as terminator of proot-distro login options processing.
+# All arguments behind it would not be treated as options of PRoot Distro.
+proot-distro login debian --shared-tmp -- /bin/bash -c  'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - droidmaster -c "env DISPLAY=:0 startplasma-x11"'
+
+exit 0
