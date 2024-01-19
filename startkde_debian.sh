ebadf66c0ea572a0f4fb142e568cf68702a96bb3
DroidMaster <155466495+LinuxDroidMaster@users.noreply.github.com>
2024-01-19 11:42:33 +0100

    Update startkde_debian.sh

--- startkde_debian.sh
+++ startkde_debian.sh
@@ -13,13 +13,15 @@ termux-x11 :0 >/dev/null &
 # Wait a bit until termux-x11 gets started.
 sleep 3
 
+# Launch Termux X11 main activity
+am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
+sleep 1
+
 # Login in PRoot Environment. Do some initialization for PulseAudio, /tmp directory
 # and run KDE as user droidmaster.
 # See also: https://github.com/termux/proot-distro
 # Argument -- acts as terminator of proot-distro login options processing.
 # All arguments behind it would not be treated as options of PRoot Distro.
-am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
-sleep 1
 proot-distro login debian --shared-tmp -- /bin/bash -c  'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - droidmaster -c "env DISPLAY=:0 startplasma-x11"'
 
 exit 0
