3682f21d2ed45647162fa3455283ff95159b8e9a
DroidMaster <155466495+LinuxDroidMaster@users.noreply.github.com>
2024-01-19 11:42:08 +0100

    Update startlxde_debian.sh

--- startlxde_debian.sh
+++ startlxde_debian.sh
@@ -1,5 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+# Kill open X11 processes
+kill -9 $(pgrep -f "termux.x11") 2>/dev/null
+
 # Enable PulseAudio over Network
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 
@@ -9,6 +12,10 @@ termux-x11 :0 >/dev/null &
 
 # Wait a bit until termux-x11 gets started.
 sleep 3
+
+# Launch Termux X11 main activity
+am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
+sleep 1
 
 # Login in PRoot Environment. Do some initialization for PulseAudio, /tmp directory
 # and run LXDE as user droidmaster.
