71e28fb6e8e7cf83b348ca47f2c4b813f127f363
DroidMaster <155466495+LinuxDroidMaster@users.noreply.github.com>
2024-04-20 09:03:47 +0200

    Update chroot_debian_installer.sh

--- scripts/chroot/debian/chroot_debian_installer.sh
+++ scripts/chroot/debian/chroot_debian_installer.sh
@@ -194,7 +194,7 @@ install_lxde() {
 download_startxfce4_script() {
     progress "Downloading startxfce4_chrootDebian.sh script..."
     if [ "$DE_OPTION" -eq 1 ]; then
-        wget -O "/data/local/tmp/startxfce4_chrootDebian.sh" "https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/chroot/debian/startxfce4_chrootDebian.sh"
+        wget -O "~/startxfce4_chrootDebian.sh" "https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/chroot/debian/startxfce4_chrootDebian.sh"
         if [ $? -eq 0 ]; then
             success "startxfce4_chrootDebian.sh script downloaded successfully"
         else
