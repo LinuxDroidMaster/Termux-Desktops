be59e24d5ceca7a205de4184c551094b6c526fcd
Carlos <linuxdroidmaster@gmail.com>
2024-07-23 19:37:10 +0200

    fix script

--- scripts/proot_void/startxfce4_void.sh
+++ scripts/proot_void/startxfce4_void.sh
@@ -7,7 +7,8 @@ kill -9 $(pgrep -f "termux.x11") 2>/dev/null
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 
 # Prepare termux-x11 session
-export XDG_RUNTIME_DIR=${TMPDIR}                                                       termux-x11 :0 >/dev/null &
+export XDG_RUNTIME_DIR=${TMPDIR}
+termux-x11 :0 >/dev/null &
 
 # Wait a bit until termux-x11 gets started.
 sleep 3
