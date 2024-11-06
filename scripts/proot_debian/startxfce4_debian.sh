b95f931e236c54183682113d1b71518c72a03c35
annisa1007 <110367924+annisa1007@users.noreply.github.com>
2024-11-06 11:15:33 +0800

    Update startxfce4_debian.sh

--- scripts/proot_debian/startxfce4_debian.sh
+++ scripts/proot_debian/startxfce4_debian.sh
@@ -22,6 +22,6 @@ sleep 1
 # See also: https://github.com/termux/proot-distro
 # Argument -- acts as terminator of proot-distro login options processing.
 # All arguments behind it would not be treated as options of PRoot Distro.
-proot-distro login debian --shared-tmp -- /bin/bash -c  'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - droidmaster -c "env DISPLAY=:0 startxfce4"'
+proot-distro login debian --shared-tmp -- /bin/bash -c  'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - annisa -c "env DISPLAY=:0 startxfce4"'
 
 exit 0
