5e7a94beb3cfb3ec4672eaccbf9c548d097355c8
annisa1007 <110367924+annisa1007@users.noreply.github.com>
2024-11-14 03:25:16 +0800

    startxfce4_void.sh

--- scripts/proot_void/startxfce4_void.sh
+++ scripts/proot_void/startxfce4_void.sh
@@ -18,10 +18,10 @@ am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
 sleep 1
 
 # Login in PRoot Environment. Do some initialization for PulseAudio, /tmp directory
-# and run XFCE4 as user droidmaster.
+# and run XFCE4 as user nisa.
 # See also: https://github.com/termux/proot-distro
 # Argument -- acts as terminator of proot-distro login options processing.
 # All arguments behind it would not be treated as options of PRoot Distro.
-proot-distro login void --shared-tmp -- /bin/bash -c  'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - droidmaster -c "env DISPLAY=:0 dbus-launch --exit-with-session startxfce4"'
+proot-distro login void --shared-tmp -- /bin/bash -c  'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - nisa -c "env DISPLAY=:0 dbus-launch --exit-with-session startxfce4"'
 
 exit 0
